6504ab0d153f6b0d780a81ce0ca3297c97e6c4a5
Glenn K. Lockwood <glennklockwood@gmail.com>
2021-01-24 22:24:28 -0800

    final commit to this script; superseded by docker-compose

--- docker_dli_run.sh
+++ docker_dli_run.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
-
-# -it = interactive
+#
+#  Run the container for the NVIDIA Deep Learning Institute course on AI for
+#  Jetson Nano.
+#
+#  This has been superseded by the docker-compose configuration in
+#  docker-compose.yml.  See that file for directions on how to run.
+#
+# --runtime nvidia = necessary default isn't set in /etc/docker/daemon.json
+# -it = interactive (use stdin, tty)
 # --rm = delete container when finished
 # --network host = container will open ports on host itself
 # --volume = bind mount
 # --device = pass USB camera into container
-
-# the specific nvcr.io path and container tag must come from:
+#
+# The specific nvcr.io path and container tag must come from:
 #
 # https://ngc.nvidia.com/catalog/containers/nvidia:dli:dli-nano-ai
-
+#
 docker run \
     --runtime nvidia \
     -it \
